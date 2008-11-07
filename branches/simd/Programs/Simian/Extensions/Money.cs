a4a3d94ec859f5dea4a4bcc80eb1f64fb7ecb3fb
jhurliman <jhurliman@52acb1d6-8a22-11de-b505-999d5b087335>
2008-11-07 03:19:42 +0000

    git-svn-id: https://libopenmetaverse.googlecode.com/svn/libopenmetaverse@2330 52acb1d6-8a22-11de-b505-999d5b087335

---
+++ branches/simd/Programs/Simian/Extensions/Money.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using ExtensionLoader;
+using OpenMetaverse;
+using OpenMetaverse.Packets;
+
+namespace Simian.Extensions
+{
+    class Money : IExtension<Simian>
+    {
+        Simian server;
+
+        public Money()
+        {
+            
+        }
+
+        public void Start(Simian server)
+        {
+            this.server = server;
+
+            server.UDP.RegisterPacketCallback(PacketType.MoneyBalanceRequest, new PacketCallback(MoneyBalanceRequestHandler));
+            server.UDP.RegisterPacketCallback(PacketType.MoneyTransferRequest, new PacketCallback(MoneyTransferRequestHandler));
+        }
+
+        public void Stop()
+        {
+        }
+
+        void SendBalance(Agent agent, UUID transactionID, string message)
+        {
+            MoneyBalanceReplyPacket reply = new MoneyBalanceReplyPacket();
+            reply.MoneyData.AgentID = agent.AgentID;
+            reply.MoneyData.MoneyBalance = agent.Balance;
+            reply.MoneyData.TransactionID = transactionID;
+            reply.MoneyData.Description = Utils.StringToBytes(message);
+
+            server.UDP.SendPacket(agent.AgentID, reply, PacketCategory.Transaction);
+        }
+
+        void MoneyBalanceRequestHandler(Packet packet, Agent agent)
+        {
+            MoneyBalanceRequestPacket request = (MoneyBalanceRequestPacket)packet;
+
+            SendBalance(agent, request.MoneyData.TransactionID, String.Empty);
+        }
+
+        void MoneyTransferRequestHandler(Packet packet, Agent agent)
+        {
+            MoneyTransferRequestPacket request = (MoneyTransferRequestPacket)packet;
+
+            if (request.MoneyData.Amount < 0 || request.MoneyData.Amount > agent.Balance)
+                return;
+
+            lock (server.Agents)
+            {
+                foreach (Agent recipient in server.Agents.Values)
+                {
+                    if (recipient.AgentID == request.MoneyData.DestID)
+                    {
+                        agent.Balance -= request.MoneyData.Amount;
+                        recipient.Balance += request.MoneyData.Amount;
+
+                        SendBalance(agent, UUID.Zero, String.Format("You paid L${0} to {1}.", request.MoneyData.Amount, recipient.Avatar.Name));
+                        SendBalance(agent, UUID.Zero, String.Format("{1} paid you L${0}.", request.MoneyData.Amount, agent.Avatar.Name));
+
+                        break;
+                    }
+                }
+            }
+        }
+        
+    }
+}
